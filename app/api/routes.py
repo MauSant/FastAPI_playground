bc9dc60480cffbb4fb1708e49c97fd2b0273964a
maucsant <maubr93@gmail.com>
2022-09-02 14:42:00 -0300

    feat: removes asyncuser endpoint

--- app/api/routes.py
+++ app/api/routes.py
@@ -12,8 +12,6 @@ api_router.include_router(user.router, prefix="/users", tags=["users"])
 
 @api_router.on_event("startup")
 def startup_db_client():
-    # api_router.mongo_client = get_mongo_client()
-    # init_db(api_router.mongo_client)
     init_db()
 
 
